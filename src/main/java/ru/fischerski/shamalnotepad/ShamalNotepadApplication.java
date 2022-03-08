547ec435a9ba623ad6593e883afa99f75aaef973
Pavel <r-half@yandex.ru>
2022-03-08 19:56:11 +0300

    Create project

---
+++ src/main/java/ru/fischerski/shamalnotepad/ShamalNotepadApplication.java
@@ -0,0 +1,13 @@
+package ru.fischerski.shamalnotepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShamalNotepadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShamalNotepadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fischerski/shamalnotepad/ShamalNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fischerski.shamalnotepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShamalNotepadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
