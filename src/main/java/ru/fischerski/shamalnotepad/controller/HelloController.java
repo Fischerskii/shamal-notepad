1d87d06523561e885f786fc64214154ff294e52e
Pavel <r-half@yandex.ru>
2022-06-11 20:00:18 +0300

    add HelloController

--- src/main/java/ru/fischerski/shamalnotepad/controller/HelloController.java
+++ src/main/java/ru/fischerski/shamalnotepad/controller/HelloController.java
@@ -1,9 +1,14 @@
 package ru.fischerski.shamalnotepad.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.fischerski.shamalnotepad.PersonDetails;
 import ru.fischerski.shamalnotepad.service.AdminService;
+import ru.fischerski.shamalnotepad.service.PersonDetailService;
 
 @Controller
 public class HelloController {
@@ -18,5 +23,20 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHell() {
         return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getUser());
+
+        return "hello";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        adminService.doAdminStuff();
+        return "admin";
     }
 }
